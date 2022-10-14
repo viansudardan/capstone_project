e3d38c29ec3a089bcb39c5a86f35614796f95c15
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-15 03:15:40 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -20,7 +20,7 @@ import plotly.express as px
 #st.title("Dampak Inflasi AS terhadap Perekonomian Indonesia")
 st.set_page_config(layout='wide')
 #st.markdown("<h2 style='text-align: center; color: black;'>Smaller headline in black </h2>", unsafe_allow_html=True)
-st.image("./inflasi peng.png")
+#st.image(style="width:100%;height:400px"./inflasi peng.png")
 
 st.markdown("<h1 style='text-align: center; color: black;'>Dampak Inflasi AS terhadap Perekonomian Indonesia</h1>", unsafe_allow_html=True)
 st.write('---')
@@ -34,7 +34,7 @@ c1, c2 = st.columns([1,2])
 with c1:
    st.markdown("Tidak terlepas dari kondisi krisis, Amerika Serikat (AS) sebagai negara superpower pun terkena imbas dari isu ini, Pada bulan Juni 2022, Biro Statistik Tenaga Kerja AS (Bureau of Labor Statistics) mencatat tingkat inflasi sempat menembus laju tertingginya sepanjang tahun 2022 pada nilai 9,1%, ini adalah level tertinggi dalam 40 tahun terakhir, bahkan jika keadaan terus memburuk tidak menutup kemungkinan akan menyebabkan resesi. Tercatat AS pernah mencatat tingkat inflasi tertinggi sepanjang sejarah sebesar 12,3% pada bulan Desember 1974. Adapun kebijakan yang diambil oleh pemerintah AS melalui Bank Sentral nya, The Federal Reserve (The Fed) pada saat itu adalah dengan menaikan suku bunga acuan. Data pada bulan September 2022 tercatat tingkat inflasi AS menurun menjadi 8,2%, namun ini masih tergolong tinggi, hal ini mendorong The Fed meningkatkan suku bunga acuan menjadi 3,25% pada bulan September 2022")
 with c2:
-   c2.markdown("<body style='border-radius: 6px; box-shadow : 0 1px 4px rgba(0,0,0,.23);></body>", unsafe_allow_html=True)
+   st.markdown("<body style='border-radius: 6px; box-shadow : 0 1px 4px rgba(0,0,0,.23);></body>", unsafe_allow_html=True)
    df = pd.read_excel('./DataInflasiAS.xlsx')
    fig = px.line(df, x='Periode', y='Nilai', markers=True, color='Rate')
    fig.update_layout(title = 'Suku Bunga Acuan The Fed & Tingkat Inflasi di AS', xaxis_title='Tahun', yaxis_title='Persentase (%)')
@@ -61,6 +61,25 @@ with c2:
 #sukubunga = df2[['Bulan', 'Suku Bunga']].set_index('Bulan')
 #st.line_chart(sukubunga)
 
+
+c3, c4 = st.columns([2,1])
+with c1:
+   df2 = pd.read_excel('./DataInflasiIND.xlsx')
+   fig2 = px.line(df2, x='Periode', y='Nilai', markers=True, color='Rate')
+   fig2.update_layout(title = 'Suku Bunga Acuan BI & Tingkat Inflasi di Indonesia', xaxis_title='Tahun', yaxis_title='Persentase (%)')
+   fig2.show()
+   st.plotly_chart(fig2, use_container_width=True)
+   st.caption("<p style='text-align: center;'>sumber :Bank Indonesia</p>", unsafe_allow_html=True)
+
+with c2:
+   st.markdown('Untuk merespon kebijakan tersebut, Bank Sentral diberbagai negara mau tidak mau juga ikut meningkatkan suku bunganya untuk menahan keluarnya arus modal asing, tercatat Bank Indonesia (BI) menaikan suku bunga acuan menjadi 4,25% pada bulan September 2022. Namun harus menjadi perhatian bagi pemerintah bahwasannya kenaikan suku bunga berarti akan memperlambat laju pertumbuhan ekonomi dan menurunkan daya beli masyarakat.')
+
+#st.subheader('Suku Bunga Acuan Bank Indonesia (BI)')
+#df4 = pd.read_excel('./suku_bunga_bi.xlsx')
+#df4['Bulan'] = pd.to_datetime(df4['Bulan'])
+#sukubungaBI = df4[['Bulan', 'Suku Bunga']].set_index('Bulan')
+#st.line_chart(sukubungaBI)
+
 st.markdown('Peningkatan suku bunga di AS dapat memicu aliran modal global akan meninggalkan negara berkembang termasuk Indonesia (capital outflow). Investor berbondong-bondong akan menginvestasikan modalnya pada pasar modal di AS, karena tergiur dengan kenaikan suku bunga tersebut. Dampak yang ditimbulkan adalah nilai tukar rupiah yang akan semakin melemah terhadap dollar AS.')
 
 st.subheader('Nilai Tukar Mata Uang Rupiah')
@@ -68,14 +87,6 @@ df3 = pd.read_excel('./Kurs_Rupiah.xlsx')
 df3['Tanggal'] = pd.to_datetime(df3['Tanggal'])
 kurs = df3[['Tanggal', 'Kurs Jual']].set_index('Tanggal')
 st.line_chart(kurs)
-
-st.markdown('Untuk merespon kebijakan tersebut, Bank Sentral diberbagai negara mau tidak mau juga ikut meningkatkan suku bunganya untuk menahan keluarnya arus modal asing, tercatat Bank Indonesia (BI) menaikan suku bunga acuan menjadi 4,25% pada bulan September 2022. Namun harus menjadi perhatian bagi pemerintah bahwasannya kenaikan suku bunga berarti akan memperlambat laju pertumbuhan ekonomi dan menurunkan daya beli masyarakat.')
-
-st.subheader('Suku Bunga Acuan Bank Indonesia (BI)')
-df4 = pd.read_excel('./suku_bunga_bi.xlsx')
-df4['Bulan'] = pd.to_datetime(df4['Bulan'])
-sukubungaBI = df4[['Bulan', 'Suku Bunga']].set_index('Bulan')
-st.line_chart(sukubungaBI)
 
 st.markdown('Dampak lainnya biaya bahan baku yang diambil dari AS atau dikirim dari AS akan mengalami kenaikan harga. Hal ini akan berimbas kepada inflasi global dikarenakan kenaikan harga ini akan meningkatkan biaya produksi sehingga produk yang dihasilkan akan mengalami kenaikan harga yang akan dibebankan kepada konsumen sehingga ada transmisi inflasi yang tinggi di AS terhadap harga produk di berbagai negara termasuk produk yang ada di Indonesia yang mengambil bahan baku dari AS.')
 
