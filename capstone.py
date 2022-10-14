6b42ac1d6bc502637382d7bc49a9af8148a53a9a
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-15 04:00:25 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -82,8 +82,8 @@ with c4:
 
 c5, c6 = st.columns([1,2])
 with c5:
-   st.markdown('Peningkatan suku bunga yang tinggi di AS dapat memicu para investor untuk menginvestasikan modal di negaranya karena tergiur dengan bunga yang tinggi, hal ini akan membuat aliran modal global meninggalkan negara yang memiliki suku bunga dibawah nilai suku bunga yang ditetapkan oleh The Fed, termasuk Indonesia.')
-   st.markdown('Dampak yang ditimbulkan adalah nilai tukar mata uang dollar AS akan semakin perkasa terhadapa mata uang lainnya, tercatat kurs mata uang rupiah terhadap dollar pada akhir bulan September adalah sebesar 15.303,70 dan tidak menutup kemungkinan akan semakin melemah jika pemerintah tidak memiliki strategi yang tepat untuk mengatasinya.')
+   st.markdown('Peningkatan suku bunga di AS akan membuat para investor menginvestasikan modalnya pada pasar AS karena tergiur dengan bunga yang tinggi, hal ini akan memicu aliran modal global meninggalkan negara-negara yang memiliki suku bunga dibawah nilai suku bunga yang ditetapkan oleh The Fed, termasuk Indonesia.')
+   st.markdown('Hal ini akan berimbas kepada nilai tukar mata uang dollar AS yang akan semakin perkasa terhadapa mata uang lainnya, tercatat kurs mata uang rupiah terhadap dollar pada akhir bulan September 2022 adalah sebesar 15.303,70 dan tidak menutup kemungkinan akan semakin melemah jika pemerintah tidak memiliki strategi yang tepat untuk mengatasinya.')
    #Namun harus menjadi perhatian bagi pemerintah bahwasannya kenaikan suku bunga berarti akan memperlambat laju pertumbuhan ekonomi dan menurunkan daya beli masyarakat.
 with c6:
    df3 = pd.read_excel('./Kurs_Rupiah.xlsx')
