204c8c8b31b0947890e611ed190b8a37cf80a531
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-14 23:20:59 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -38,7 +38,7 @@ df = pd.read_excel('./DataInflasiAS.xlsx')
 #df = px.data.gapminder().query("continent=='Oceania'")
 #fig = px.line(df, x = df['Periode'], y = df['Data Inflasi'], title = 'Data Inflasi AS')
 df.head()
-fig = px.line(df, x='Periode', y='Data Inflasi')
+fig = px.line(df, x='Periode', y='Data Inflasi', markers=True)
 fig.show()
 st.plotly_chart(fig, use_container_width=True)
 
