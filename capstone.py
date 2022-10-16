10838bd367cfc597656294b799768bf58e5312cb
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-16 19:58:23 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -112,14 +112,12 @@ with c6:
       go.Scatter(x=list(df3.Date), 
               y=list(df3.decrease_price),
               name="Valuasi",
-              color='Currency'
               #line=dict(color="#33CFA5")
               ))
    fig.add_trace(
       go.Scatter(x=list(df3.Date), 
               y=list(df3.close_price),
               name="Kurs",
-              color='Currency'
               #line=dict(color="#F06A6A")
               ))
 
@@ -177,6 +175,9 @@ with c6:
    #fig3.update_layout(title = 'Nilai Mata Uang Dunia Terhadap Dollar AS', title_font_size = 20, paper_bgcolor = "#c7d7e8", xaxis_title='Periode', yaxis_title='Nilai')
    #fig3.show()
    #st.plotly_chart(fig3, use_container_width=True)
+   fig3 = px.line(df3, x='Date', y='decrease_price', markers=False, color='Currency')
+   fig3.update_layout(title = 'Nilai Valuasi Mata Uang Dunia Terhadap Dollar AS', xaxis_title='Periode', yaxis_title='Nilai')
+   fig3.show()
    st.plotly_chart(fig3)
 
    #tickerSymbol = st.radio("Pick Stock",["GOOGL", "AAPL"])
