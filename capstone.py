d19bcbeefe451c2c9ef6403d9cfccdf3ca5bbdd8
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-16 17:18:35 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -104,8 +104,7 @@ with c6:
    fig4 = px.line(df4, x='Date', y='Close', markers=False, color='Currency')
 
 
-   Nilai = st.radio(["Valuasi", "Kurs"])
-   Option = (Nilai)
+   Nilai = st.radio("Valuasi", "Kurs")
    if Nilai == "Valuasi":
       fig3.update_layout(title = 'Nilai Mata Uang Dunia Terhadap Dollar AS', title_font_size = 20, paper_bgcolor = "#c7d7e8", xaxis_title='Periode', yaxis_title='Nilai'),
       fig3.show(),
