88b55c8784e8595e201cb67118634c6f211b83c5
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-17 01:43:19 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -49,7 +49,7 @@ with c6:
    df7 = pd.read_csv('./aud_rate.csv')
    df8 = pd.read_csv('./cny_rate.csv')
    df9 = pd.read_csv('./chf_rate.csv')
-   #df10 = pd.read_csv('./open_rate.csv')
+   df10 = pd.read_csv('./open_rate.csv')
    #fig3 = px.line(df10, x='Date', y='close_price', markers=False)
    #fig3.update_layout(title = 'Kurs Mata Uang Dunia Terhadap Dollar AS', title_font_size = 20, paper_bgcolor = "#e4e4fe", xaxis_title='Tanggal', yaxis_title='Nilai')
    #fig3.show()
@@ -196,10 +196,10 @@ with c6:
               ),
       ]
    )
-
-   #fig3 = px.line(df3, x='Date', y='close_price', markers=False, color='Negara')
+   
+   fig3 = px.line(df10, x='Date', y='close_price', markers=False, color='Negara')
    fig3.update_layout(title = 'Nilai Valuasi Mata Uang Dunia Terhadap Dollar AS', title_font_size = 20, paper_bgcolor = "#e4e4fe", xaxis_title='Tanggal', yaxis_title='Nilai')
-   #fig3.show()
+   fig3.show()
    st.plotly_chart(fig3, use_container_width=True)
    st.caption("<p style='text-align: center;'>Sumber : Yahoo Finance</p>", unsafe_allow_html=True)
 
