98412837afc4ad27fdbe44891a34a9876f073455
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-17 21:56:58 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -219,7 +219,7 @@ with c8:
    df12 = pd.read_excel('./pdb_ekspor.xlsx')
    df13 = pd.read_excel('./pdb_impor.xlsx')
    #fig5 = go.Figure()
-   fig5 = px.bar(df12, x="Periode", y="Nilai", title="Ekspor dan Impor Indonesia")
+   fig5 = px.bar(df13, x="Periode", y="Nilai", title="Ekspor dan Impor Indonesia")
    #fig5 = px.bar(df12, x="Periode", y="Nilai", title="Ekspor dan Impor Indonesia")
    #fig5.add_trace(
    #   go.Bar(x=list[df12.Periode],
