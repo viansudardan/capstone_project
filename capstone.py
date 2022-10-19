30a83c7d303a1ae1d235e2251899dee8a2fc67f8
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-20 02:15:54 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -6,7 +6,7 @@ import yfinance as yf
 
 st.set_page_config(layout='wide')
 with open('style.css') as f:
-   st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+   st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: black;'>Dampak Inflasi AS terhadap Perekonomian Indonesia</h1>", unsafe_allow_html=True)
 st.write('---')
