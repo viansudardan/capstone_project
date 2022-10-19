9a1cd1c880319c90dda46ddca07e828fbd97f9c7
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-20 00:07:32 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -195,14 +195,14 @@ with c5:
    fig4.add_trace(
       go.Scatter(x=list(df3.Date), 
                  y=list(df3.decrease_price),
-                 name="Kurs Indonesia",
+                 name="Indonesia",
                  line=dict(color="#33CFA5")
               ))
    #Figure Kurs
    fig4.add_trace(
       go.Scatter(x=list(df3.Date), 
                  y=list(df3.close_price),
-                 name="Valuasi Indonesia",
+                 name="Indonesia",
                  line=dict(color="#e30202"),
                  visible = False
               ))
@@ -256,7 +256,7 @@ with c5:
 
 
 with c6: 
-   st.markdown('Kurs mata uang Rupiah melemah terhadap dollar AS sudah lebih dari 1.000 Rupiah semenjak tahun lalu, tercatat pada akhir bulan September 2022 kurs Rupiah sebesar 15.175,20. Tidak menutup kemungkinan akan semakin melemah jika pemerintah tidak memiliki strategi yang tepat untuk mengatasinya. Harus menjadi perhatian bagi pemerintah bahwasannya menaikan suku bunga dapat memperlambat laju pertumbuhan ekonomi dan menurunkan daya beli masyarakat.')
+   st.markdown('Begitu juga dengan nilai mata uang Rupiah yanag melemah terhadap dollar AS, sampai dengan saat ini valusai mata uang Rupiah sudah melemah lebih dari 1.000 Rupiah semenjak tahun lalu, tercatat pada akhir bulan September 2022 kurs Rupiah sebesar 15.175,20. Tidak menutup kemungkinan akan semakin melemah jika pemerintah tidak memiliki strategi yang tepat untuk mengatasinya.')
 
 st.markdown('Inflasi yang terjadi di AS juga dapat mengganggu kinerja ekspor Indonesia. Jika konsumsi rumah tangga di AS menurun, maka hal ini dapat mempengaruhi demand dari komoditas ekspor Indonesia yang juga akan mengalami penurunan sehingga devisa negara juga akan mengalami penurunan.')
 st.markdown('Namun, pada kali ini Pemerintah Indonesia terselamatkan karena harga komoditas ekspor seperti kelapa sawit dan batu bara mengalami kenaikan sehingga kinerja perdagangan luar negeri masih tumbuh secara positif ditengah tekanan ekonomi global. Pertumbuhan ekonomi Indonesia pada kuartal II tahun 2022 cukup impresif berada di angka 5,4%, dan pertumbuhan Indonesia pada kuartal III tahun 2022 diproyeksikan akan meningkat sebesar 0,1% yaitu berada di angka 5,5%.')
