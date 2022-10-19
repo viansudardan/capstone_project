d0de38f7b870666ceb64be41f3875ec80fd9419d
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-20 00:34:59 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -256,12 +256,8 @@ with c5:
 
 
 with c6: 
-   container = st.container()
+   container = st.container(color='#787d7b')
    container.write('Begitu juga dengan nilai mata uang Rupiah yanag melemah terhadap dollar AS, sampai dengan saat ini valusai mata uang Rupiah sudah melemah lebih dari 1.000 Rupiah semenjak tahun lalu, tercatat pada akhir bulan September 2022 kurs Rupiah sebesar 15.175,20. Tidak menutup kemungkinan akan semakin melemah jika pemerintah tidak memiliki strategi yang tepat untuk mengatasinya.')
-   st.write("This is outside the container")
-
-   # Now insert some more in the container
-   container.write("This is inside too")
 
 st.markdown('Inflasi yang terjadi di AS juga dapat mengganggu kinerja ekspor Indonesia. Jika konsumsi rumah tangga di AS menurun, maka hal ini dapat mempengaruhi demand dari komoditas ekspor Indonesia yang juga akan mengalami penurunan sehingga devisa negara juga akan mengalami penurunan.')
 st.markdown('Namun, pada kali ini Pemerintah Indonesia terselamatkan karena harga komoditas ekspor seperti kelapa sawit dan batu bara mengalami kenaikan sehingga kinerja perdagangan luar negeri masih tumbuh secara positif ditengah tekanan ekonomi global. Pertumbuhan ekonomi Indonesia pada kuartal II tahun 2022 cukup impresif berada di angka 5,4%, dan pertumbuhan Indonesia pada kuartal III tahun 2022 diproyeksikan akan meningkat sebesar 0,1% yaitu berada di angka 5,5%.')
