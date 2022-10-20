d4f782f3d3fda01bd2d01fbddfb8f7021d605650
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-20 09:12:48 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -5,6 +5,15 @@ import plotly.graph_objects as go
 import yfinance as yf
 
 st.set_page_config(layout='wide')
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#col1, col2, col3 = st.columns(3)
+##col1.metric("Temperature", "70 °F", "1.2 °F")
+#col2.metric("Wind", "9 mph", "-8%")
+#col3.metric("Humidity", "86%", "4%")
+
 st.markdown("<h1 style='text-align: center; color: black;'>Dampak Inflasi AS terhadap Perekonomian Indonesia</h1>", unsafe_allow_html=True)
 st.write('---')
 st.write("""
