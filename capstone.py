b43f874cd8c6cb93d1fe796480cb94412dc5655a
viansudardan <115474382+viansudardan@users.noreply.github.com>
2022-10-20 12:46:04 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -44,7 +44,7 @@ with c4:
    df6 = pd.read_csv('./eur_rate.csv')
    df7 = pd.read_csv('./aud_rate.csv')
    df8 = pd.read_csv('./chf_rate.csv')
-   df9 = pd.read_csv('./kwd_rate.csv')
+   df9 = pd.read_csv('./cny_rate.csv')
    #df10 = pd.read_csv('./open_rate.csv')
    
    #initialize figure
@@ -82,7 +82,7 @@ with c4:
    fig3.add_trace(
       go.Scatter(x=list(df9.Date), 
                  y=list(df9.decrease_price),
-                 name="Kuwait",
+                 name="China",
                  line=dict(color="#0a704b")
               ))
    fig3.add_trace(
@@ -124,7 +124,7 @@ with c4:
    fig3.add_trace(
       go.Scatter(x=list(df9.Date), 
                  y=list(df9.close_price),
-                 name="Kuwait",
+                 name="China",
                  line=dict(color="#0a704b"),
                  visible = False
               ))
